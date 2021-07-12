2f8e92131523e3b648ffd4ca3b03a35eb1fdbc21
Eduardo Edson Batista Cordeiro Alves <edu-23@hotmail.com>
2021-07-12 20:14:21 -0300

    change backend route

--- src/services/Backend.js
+++ src/services/Backend.js
@@ -1,4 +1,3 @@
 import axios from 'axios'
-const Backend = axios.create({ baseURL: 'http://127.0.0.1:3333' })
-// const Backend = axios.create({ baseURL: 'https://facturas-backend.herokuapp.com' })
+const Backend = axios.create({ baseURL: 'https://facturas-backend.herokuapp.com' })
 export default Backend
